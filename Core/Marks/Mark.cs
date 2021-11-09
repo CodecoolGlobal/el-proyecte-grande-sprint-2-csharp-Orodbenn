7a78ed35311f8802cc1d8094ed5e2d58b93c0b78
Orodben <esztermanna@gmail.com>
2021-11-09 14:04:41 +0100

    feat: creates features of Marks/Mark.cs

--- Core/Marks/Mark.cs
+++ Core/Marks/Mark.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Users;
 
 namespace Core.Marks
 {
    public class Mark
-    {
+   {
+       private byte mark;
+       private Teacher assigningTeacher;
+       private Subject subject;
+       private DateTime dateOfAssignment;
+       private MarkWeight weight;
 
-    }
+       public Mark(byte mark, Teacher assigningTeacher, Subject subject, MarkWeight weight)
+       {
+           markValidation(mark);
+           this.assigningTeacher = assigningTeacher;
+           this.subject = subject;
+           this.weight = weight;
+           dateOfAssignment = DateTime.Now;
+       }
+
+       private void markValidation(byte mark)
+       {
+           if (mark >= 1 || mark <= 5)
+           {
+               this.mark = mark;
+           }
+           else
+           {
+               throw new Exception(); // TODO implement exception
+           }
+       }
+   }
 }
