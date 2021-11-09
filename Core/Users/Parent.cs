31a13da8b0282f7aeebab87178d5ea6c5d70f94a
NGergely7712 <nagy.gergely19991105@gmail.com>
2021-11-09 14:34:31 +0100

    feat: Implement add and remove student methods

--- Core/Users/Parent.cs
+++ Core/Users/Parent.cs
@@ -8,11 +8,23 @@ namespace Core.Users
 {
     class Parent : User
     {
-        public HashSet<Student> students { get; set; }
+        public HashSet<Student> students = new HashSet<Student>();
 
         public Parent(string name, int age, string phoneNumber, string personalId) : base(name, age, phoneNumber, personalId)
         {
-            students = new HashSet<Student>();
+            
         }
+        
+        public void addStudent(Student studentToAdd)
+        {
+            students.Add(studentToAdd);
+        }
+
+        public void removeStudent(Student studentToRemove)
+        {
+            students.Remove(studentToRemove);
+        }
+
+
     }
 }
