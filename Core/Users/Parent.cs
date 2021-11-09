db5f799b8653d823a13637fa54d9dc7854cc3f35
NGergely7712 <nagy.gergely19991105@gmail.com>
2021-11-09 14:24:58 +0100

    feat: Create parent specific field

--- Core/Users/Parent.cs
+++ Core/Users/Parent.cs
@@ -8,6 +8,8 @@ namespace Core.Users
 {
     class Parent : User
     {
+        public HashSet<Student> students { get; set; }
+
         public Parent(string name, int age, string phoneNumber, string personalId) : base(name, age, phoneNumber, personalId)
         {
         }
