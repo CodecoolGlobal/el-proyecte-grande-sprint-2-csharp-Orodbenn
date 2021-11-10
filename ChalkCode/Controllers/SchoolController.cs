c3df721e22bfa7b8e98a2808329b7cb9900cfcf9
Orodben <esztermanna@gmail.com>
2021-11-10 14:59:30 +0100

    feat: adds http endpoints

---
+++ ChalkCode/Controllers/SchoolController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Json;
+using Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChalkCode.Controllers
+{
+    [ApiController]
+    public class SchoolController
+    {
+        [Route("/classes")]
+        [HttpGet]
+        public HashSet<SchoolClass> GetClasses()
+        {
+            return null; // school.classesInTheSchool;
+        }
+        
+        [Route("/classes")]
+        [HttpPost]
+        public void AddClasses([FromBody] JsonContent body)
+        {
+            
+            // school.addNewClasses(int, int)
+        }
+    }
+}
