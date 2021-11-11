55b6fd23aae710f32420bd4e7317f0fe46cdeb92
NGergely7712 <nagy.gergely19991105@gmail.com>
2021-11-11 11:43:50 +0100

    chore: Create Teacher Controller class

---
+++ ChalkCode/Controllers/TeacherController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Users;
+using Core;
+
+namespace ChalkCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        Teacher Teacher = new Teacher("Teacher Bob");
+
+        /*
+        [HttpGet]
+        [Route("/showHomeworks")]
+        public Dictionary<StudentClass, Dictionary<DateTime, string>> showHomeworks()
+        {
+            return Teacher.getHomeworks();
+        }
+
+        */
+
+    }
+}
